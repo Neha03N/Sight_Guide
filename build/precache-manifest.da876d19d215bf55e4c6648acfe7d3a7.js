6962080cd990d7b58fac62a2b3cf6cc01923b503
Neha03N <nikhadeneha4@gmail.com>
2024-06-02 22:13:11 +0530

    first commit

---
+++ build/precache-manifest.da876d19d215bf55e4c6648acfe7d3a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da971c8dccef8247898be63979d9c7a8",
+    "url": "/Sight_guide/index.html"
+  },
+  {
+    "revision": "3a77f4ba050adedb53bb",
+    "url": "/Sight_guide/static/css/main.3d0e70e7.chunk.css"
+  },
+  {
+    "revision": "f6857d3e508e22bf22d2",
+    "url": "/Sight_guide/static/js/2.96e96a09.chunk.js"
+  },
+  {
+    "revision": "74f6145a49ba54ec59c98ae6568fb9bb",
+    "url": "/Sight_guide/static/js/2.96e96a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a77f4ba050adedb53bb",
+    "url": "/Sight_guide/static/js/main.bea54896.chunk.js"
+  },
+  {
+    "revision": "3c7e751a9b0d5228fd4c",
+    "url": "/Sight_guide/static/js/runtime-main.407d7c3d.js"
+  }
+]);
